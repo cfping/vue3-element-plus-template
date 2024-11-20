539e806409207c598690ffa7fd5b2bae7f26b0ff
cfping <869232913@qq.com>
2024-11-20 13:19:53 +0800

    feat:添加locale语言切换全局监听

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import En from 'element-plus/es/locale/lang/en';
+
+const language = ref('zh-cn'); // 默认语言为中文
+const locale = computed(() => (language.value === 'zh-cn' ? zhCn : En)); // 根据语言动态切换
+
+// 添加语言切换事件监听器
+window.addEventListener(
+  'switchLanguage',
+  (evt: Event) => {
+    const customEvent = evt as CustomEvent; // 类型断言为 CustomEvent
+    if (customEvent.detail?.language) {
+      language.value = customEvent.detail.language; // 更新语言
+    }
+  }
+);
+//监听全局语言切换变化
+</script>
 
 <template>
-  <el-config-provider :locale="zhCn">
+  <el-config-provider :locale="locale">
     <RouterView></RouterView>
   </el-config-provider>
 </template>
